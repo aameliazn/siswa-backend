01e27649137dbcdca0e74d887fa9f5e744a2ce29
aameliazn <aameliazulkarnaen@gmail.com>
2023-10-01 20:23:50 +0700

    update routes

--- index.js
+++ index.js
@@ -8,24 +8,60 @@ const response = require("./response");
 app.use(bodyParser.json());
 
 app.get("/api/siswa", (req, res) => {
-  response(404, "not found", "lalala", res);
+  const sql = "SELECT * FROM m_expressjs_first.t_siswa ORDER BY id ASC";
+  client.query(sql, (err, fields) => {
+    if (err) throw err;
+    response(200, fields?.rows, "get all data siswa", res);
+  });
 });
 
 app.get("/api/siswa/:nis", (req, res) => {
   const nis = req?.params?.nis;
-  response(200, "data spesifik siswa", `spesifik siswa by nis ${nis}`, res);
+  const sql = `SELECT * FROM m_expressjs_first.t_siswa WHERE nis = ${nis}`;
+  client.query(sql, (err, fields) => {
+    if (err) throw err;
+    response(200, fields?.rows, "get detail siswa", res);
+  });
 });
 
 app.post("/api/siswa", (req, res) => {
-  response(200, "ini data post", "ini post", res);
+  const { nis, name, class_, address } = req?.body;
+  const sql = `INSERT INTO m_expressjs_first.t_siswa (nis, name, class, address) VALUES (${nis}, '${name}', '${class_}', '${address}')`;
+  client.query(sql, (err, fields) => {
+    const data = { isSuccess: fields?.rowCount || 0 };
+    if (err) response(500, data, `${err?.detail || err?.routine}`, res);
+    if (fields?.rowCount) {
+      response(200, data, "successfully added data", res);
+    }
+  });
 });
 
 app.put("/api/siswa", (req, res) => {
-  response(200, "ini data put", "ini put", res);
+  const { nis, name, class_, address } = req?.body;
+  const sql = `UPDATE m_expressjs_first.t_siswa SET name = '${name}', class = '${class_}', address = '${address}' WHERE nis = ${nis}`;
+  client.query(sql, (err, fields) => {
+    const data = { isSuccess: fields?.rowCount || 0 };
+    if (err) response(500, data, `${err?.detail || err?.routine}`, res);
+    if (fields?.rowCount) {
+      response(200, data, "successfully updated data", res);
+    } else {
+      response(404, data, "user not found", res);
+    }
+  });
 });
 
 app.delete("/api/siswa", (req, res) => {
-  response(200, "ini data delete", "ini delete", res);
+  const { nis } = req?.body;
+  const sql = `DELETE FROM m_expressjs_first.t_siswa WHERE nis = ${nis}`;
+  client.query(sql, (err, fields) => {
+    const data = { isSuccess: fields?.rowCount || 0 };
+    if (err) response(500, data, `${err?.detail || err?.routine}`, res);
+    if (fields?.rowCount) {
+      response(200, data, "successfully deleted data", res);
+    } else {
+      response(404, data, "user not found", res);
+    }
+  });
 });
 
 app.listen(port, () => {
